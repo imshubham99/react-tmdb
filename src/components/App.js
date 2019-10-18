16251e5b3d734883687fdd92577d9a3c07218b59
Shubham Rathod <shubhamrathod10599@gmail.com>
2019-10-18 20:01:00 +0530

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
   }
   render() {
     return (
-      <Router history={history} basename="discover/popular">
+      <Router history={history} basename="react-tmdb">
         <div className={classes}>
           <Navigation {...this.props} />
           <Route exact path="/discover/:type" component={Discover} />
