ff11e346e2e59177610103e2d1b485685f783389
Shubham Rathod <shubhamrathod10599@gmail.com>
2019-10-18 22:41:35 +0530

    Initial Commit

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -21,7 +21,7 @@ class Sidebar extends React.Component {
   renderGenres() {
     return this.props.genres.map(genre => (
       <NavLink
-        to={`${process.env.PUBLIC_URL}/genres/${genre.id}`}
+        to={`/genres/${genre.id}`}
         key={genre.id}
         className={classes.linkItem}
         activeStyle={{
@@ -142,7 +142,7 @@ class Sidebar extends React.Component {
           <NavLink
             onClick={() => this.closeMenu()}
             className={classes.linkItem}
-            to={process.env.PUBLIC_URL + "/discover/popular"}
+            to="/discover/popular"
             exact
             activeStyle={{
               color: "#00D47f",
@@ -156,7 +156,7 @@ class Sidebar extends React.Component {
           <NavLink
             onClick={() => this.closeMenu()}
             className={classes.linkItem}
-            to={process.env.PUBLIC_URL + "/discover/top_rated"}
+            to="/discover/top_rated"
             exact
             activeStyle={{
               color: "#00D47f",
@@ -170,7 +170,7 @@ class Sidebar extends React.Component {
           <NavLink
             onClick={() => this.closeMenu()}
             className={classes.linkItem}
-            to={process.env.PUBLIC_URL + "/discover/upcoming"}
+            to="/discover/upcoming"
             exact
             activeStyle={{
               color: "#00D47f",
