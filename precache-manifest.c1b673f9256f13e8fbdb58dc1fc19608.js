47d42d0de3a3fe9cdc2c8d05c90ebc42f204a232
Shubham Rathod <shubhamrathod10599@gmail.com>
2019-10-18 19:56:39 +0530

    Updates

---
+++ precache-manifest.c1b673f9256f13e8fbdb58dc1fc19608.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690b6f4761a6657a07ed8dd4b9ee039d",
+    "url": "/tmdb-react/index.html"
+  },
+  {
+    "revision": "6706a92df71118991217",
+    "url": "/tmdb-react/static/css/2.52effe29.chunk.css"
+  },
+  {
+    "revision": "8d775730d30197a183ef",
+    "url": "/tmdb-react/static/css/main.19ca59fd.chunk.css"
+  },
+  {
+    "revision": "6706a92df71118991217",
+    "url": "/tmdb-react/static/js/2.7f909403.chunk.js"
+  },
+  {
+    "revision": "8d775730d30197a183ef",
+    "url": "/tmdb-react/static/js/main.792e4b11.chunk.js"
+  },
+  {
+    "revision": "a56f4a42b96cfd3c9620",
+    "url": "/tmdb-react/static/js/runtime~main.c96e6377.js"
+  },
+  {
+    "revision": "d93e191e22006cd803dc8e48f1e3e4a8",
+    "url": "/tmdb-react/static/media/avatar.d93e191e.svg"
+  },
+  {
+    "revision": "a8104faff6806476b8165a2c3d20fe77",
+    "url": "/tmdb-react/static/media/notFound.a8104faf.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/tmdb-react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/tmdb-react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/tmdb-react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/tmdb-react/static/media/slick.f97e3bbf.svg"
+  }
+]);
