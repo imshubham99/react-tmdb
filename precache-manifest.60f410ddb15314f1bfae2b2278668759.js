ec5b7aae218e991bbc67f531114e4367dcba8ee1
Shubham Rathod <shubhamrathod10599@gmail.com>
2019-10-18 22:37:49 +0530

    Updates

--- precache-manifest.60f410ddb15314f1bfae2b2278668759.js
+++ precache-manifest.60f410ddb15314f1bfae2b2278668759.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "848d2c2fa37a45089e58e218b7ca9eec",
+    "revision": "1fc4d16c92e938a3d48354c63f1b344e",
     "url": "/react-tmdb/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-tmdb/static/css/2.1b431f39.chunk.css"
   },
   {
-    "revision": "4b9b55b3f3db9d083e44",
+    "revision": "380e76e92efd734b5d29",
     "url": "/react-tmdb/static/css/main.19ca59fd.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-tmdb/static/js/2.7f909403.chunk.js"
   },
   {
-    "revision": "4b9b55b3f3db9d083e44",
-    "url": "/react-tmdb/static/js/main.5ed67ddb.chunk.js"
+    "revision": "380e76e92efd734b5d29",
+    "url": "/react-tmdb/static/js/main.52ad48d6.chunk.js"
   },
   {
     "revision": "677d9621d18973333028",
